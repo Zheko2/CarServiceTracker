8e40b529e72e675b97f2327cca10125c269d3607
Жеко Чернев <warioor.bg@gmail.com>
2026-02-16 22:03:51 +0200

    Home page changes, Home controller touches, Privacy and Usage finished. Site.css upgrades.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,32 +1,29 @@
-using System.Diagnostics;
-using CarServiceTracker.Models;
+using CarServiceTracker.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CarServiceTracker.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            ViewBag.CarsCount = await _context.Cars.CountAsync();
+            ViewBag.ServiceRecordsCount = await _context.ServiceRecords.CountAsync();
+
             return View();
         }
 
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
