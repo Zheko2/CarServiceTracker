ef7d077fd6d6055e0b7b4cae475056892fca5a97
Жеко Чернев <warioor.bg@gmail.com>
2026-04-06 21:18:53 +0300

    Add status property to Car

--- Models/Car.cs
+++ Models/Car.cs
@@ -4,24 +4,26 @@ namespace CarServiceTracker.Models
 {
     public class Car
     {
-
-        public ICollection<Expense> Expenses { get; set; } = new List<Expense>();
-        public int? GarageId { get; set; }
-
-        public Garage? Garage { get; set; }
-
         public int Id { get; set; }
 
         [Required]
+        [StringLength(50)]
         public string Brand { get; set; } = null!;
 
         [Required]
+        [StringLength(50)]
         public string Model { get; set; } = null!;
 
-        [Range(1886, 2100, ErrorMessage = "Year must be between 1886 and 2100.")]
+        [Range(1900, 2100)]
         public int Year { get; set; }
 
-        public ICollection<ServiceRecord> ServiceRecords { get; set; }
-            = new List<ServiceRecord>();
+        [StringLength(30)]
+        public string Status { get; set; } = "Repairing";
+
+        public int? GarageId { get; set; }
+        public Garage? Garage { get; set; }
+
+        public ICollection<ServiceRecord> ServiceRecords { get; set; } = new List<ServiceRecord>();
+        public ICollection<Expense> Expenses { get; set; } = new List<Expense>();
     }
 }
